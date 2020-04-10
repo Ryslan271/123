745f57d095aa4178d07937997c8a43db9fa3980e
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-10 16:12:05 +0300

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -32,19 +32,8 @@ def regist():
 
 @app.route("/regi")
 def regi():
-    con = psycopg2.connect(
-        database="postgres",
-        user="postgres",
-        password="Qazwsxedc",
-        host="127.0.0.1",
-        port="5432"
-    )
-    cur = con.cursor()
-    cur.execute('''CREATE TABLE STUDENT 
-    (Mail TEXT PRIMARY KEY NOT NULL, 
-    Power INT NOT NULL);''')
-    con.commit()
-    con.close()
+    return render_template('Regi.py')
+    return render_template('Contact.html')
 
 
 if __name__ == '__main__':
