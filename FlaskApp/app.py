78c9a27d63f8f84f483bdb559ed8581b0b670d2b
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-11 10:46:12 +0300

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 import os
 
 app = Flask(__name__)
@@ -29,9 +32,19 @@ def regist():
     return render_template('regist.html')
 
 
-@app.route("/regi")
-def regi():
-    return render_template('Regi.py, Contact.html')
+@app.route('/regi', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('Regist.html', title='Авторизация', form=form)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 if __name__ == '__main__':
