4e199d32f1d4f39bceaa3dc44c5dc6bac752a935
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-11 11:24:07 +0300

    Add files via upload

---
+++ FlaskApp/Con1.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+def log():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('Regist.html', title='Авторизация', form=form)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
