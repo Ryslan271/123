b4b1906856106b41580df412f924a0eff1a10650
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-16 20:40:11 +0300

    Update loginform.py

--- FlaskApp/loginform.py
+++ FlaskApp/loginform.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+import sqlite3
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,14 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+    conn = sqlite3.connect("One.db")
+    cursor = conn.cursor()
+    
+    def log(self, username, password):
+        self.username = username
+        self.password = password
+        cursor.execute("""CREATE  TABLE IF NOT All
+                              (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                               email TEXT username,
+                               password1 TEXT password
+                               """)
