0240cb7e83dc4890070ca4fc8580d71a8f299ef3
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-17 13:39:36 +0300

    Update loginform.py

--- FlaskApp/loginform.py
+++ FlaskApp/loginform.py
@@ -1,6 +1,8 @@
+from flask import render_template, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+import sqlite3 as lite
 import sqlite3
 
 
@@ -9,14 +11,16 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
-    conn = sqlite3.connect("One.db")
-    cursor = conn.cursor()
-    
-    def log(self, username, password):
+
+    def log(self, username):
         self.username = username
-        self.password = password
-        cursor.execute("""CREATE  TABLE IF NOT All
-                              (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                               email TEXT username,
-                               password1 TEXT password
-                               """)
+        conn = sqlite3.connect("One.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT login FROM All")
+        rows = cursor.fetchall()
+
+        for row in rows:
+            if row == self.username:
+                return render_template("Osnova.html")
+            else:
+                return render_template(url_for('register'))
