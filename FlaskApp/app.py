72d256567d0416521f6c8d6f190a778429ecbd96
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-17 13:37:55 +0300

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request, redirect
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from flask import Flask, render_template, request, redirect, flash, url_for
+from flask_login import login_user, login_required
 import os
-import sqlite3
 from loginform import LoginForm
+import sqlite3
+
 
 conn = sqlite3.connect("One.db")
 cursor = conn.cursor()
@@ -18,26 +17,60 @@ def home():
 
 
 @app.route("/Lyshee")
+@login_required
 def lyshee():
     return render_template('Lyshee.html')
 
 
 @app.route("/O nas")
+@login_required
 def onas():
     return render_template('O nas.html')
 
 
 @app.route("/Contact")
+@login_required
 def contact():
     return render_template('Contact.html')
 
 
-@app.route('/regist', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
-        return redirect('/')
+        return redirect('/login')
+        next_page = request.args.get('next')
     return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/regist', methods=['GET', 'POST'])
+def register():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
+    if request.method == 'POST':
+        if not (login or password or password2):
+            flash('пожалуйста заполните все поля')
+        elif password != password2:
+            flash('пароли не совпадают')
+        else:
+            conn = sqlite3.connect("One.db")
+            cursor = conn.cursor()
+            cursor.execute("""UPDATE TABLE All
+                                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                 login TEXT username,
+                                password TEXT password
+                                """)
+    return render_template("Regist.html")
+
+
+@app.after_request
+def redirect_to_signin(response):
+    if response.status.code == 401:
+        return redirect(url_for('login') + '?next=' + request.url)
+    return response
 
 
 if __name__ == '__main__':
