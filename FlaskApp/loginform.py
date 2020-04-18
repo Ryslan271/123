710d85b2ad19d413d88deb5bb03263e38737622f
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-18 19:37:47 +0300

    Update loginform.py

--- FlaskApp/loginform.py
+++ FlaskApp/loginform.py
@@ -13,14 +13,22 @@ class LoginForm(FlaskForm):
         password = PasswordField('Пароль', validators=[DataRequired()])
         remember_me = BooleanField('Запомнить меня')
         submit = SubmitField('Войти')
+        self.username = username
+        self.password = password
+        self.remember_me = remember_me
+        self.submit = submit
+
+    @property
+    def log(self):
         conn = sqlite3.connect("One.db")
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM employees')
         rows = cursor.fetchall()
 
         for row in rows:
-            if row == username:
+            if row == self.username:
                 return render_template("Osnova.html")
             else:
                 return render_template(url_for('register'))
+        return iter(itervalues(self._fields))
 
