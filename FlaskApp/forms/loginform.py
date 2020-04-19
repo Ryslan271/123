ec62bcc540bc98ddb91ff1fa515f7674e46c58ba
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-04-19 17:21:14 +0300

    Update loginform.py

--- FlaskApp/forms/loginform.py
+++ FlaskApp/forms/loginform.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 from flask import render_template, redirect
-from flask_sqlalchemy import itervalues
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
@@ -25,6 +24,10 @@ def login():
         return redirect('/regist')
 
 
+def per():
+    return redirect('/login')
+
+
 class LoginForm(FlaskForm):
     global f
     username = StringField('Логин', validators=[DataRequired()])
@@ -33,7 +36,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Войти')
     conn = sqlite3.connect("One.db")
     cursor = conn.cursor()
-    cursor.execute('SELECT login1 FROM employees')
+    cursor.execute('SELECT * FROM employees')
     rows = cursor.fetchall()
 
     for row in rows:
@@ -41,3 +44,19 @@ class LoginForm(FlaskForm):
             f = True
             break
     login()
+
+
+class Reg(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+    conn = sqlite3.connect("One.db")
+    cursor = conn.cursor()
+    cursor.execute("""UPDATE employees
+                                SET login1 = 'username', 
+                                password1 = 'password'
+                                """)
+    conn.commit()
+    conn.close()
+    per()
